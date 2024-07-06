bb1da721df0f41b0817aed6777fb183d29a235e3
Swati Jain <jain.swati71@gmail.com>
2024-07-06 20:09:18 +0200

    added button and column component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Column } from "./components/column/column";
+
 function App() {
-  return <div>Kanban Board</div>;
+  return (
+    <div>
+      Kanban Board
+      <Column />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/column/column.tsx
@@ -0,0 +1,9 @@
+export function Column() {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="flex justify-between px-6 py-4">
+        <div className="font-bold text-xl mb-2">Todo</div>
+      </div>
+    </div>
+  );
+}
